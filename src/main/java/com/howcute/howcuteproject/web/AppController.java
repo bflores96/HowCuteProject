f271c633c5952a62581d2ed8ab3b37d2e8f935eb
bflores96 <103462138+bflores96@users.noreply.github.com>
2022-11-18 19:06:52 +0100

    properties, HTML, CSS updated some tests

--- src/main/java/com/howcute/howcuteproject/web/AppController.java
+++ src/main/java/com/howcute/howcuteproject/web/AppController.java
@@ -1,12 +1,27 @@
 package com.howcute.howcuteproject.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
 
 @Controller
 public class AppController {
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("welcome", "page");
+
+    return "welcomepage";
+
+    }
+
+    @GetMapping("/locale")
+    public String changeLocale(@RequestParam String language){
+        System.out.println("language selected = " + language);
+
         return "welcomepage";
     }
 
